663f9492bbe68178c088b6b2e6ecbd44495ce8ce
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-17 20:52:12 +0530

    tenth

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ myclient = boto3.client('s3',region_name='us-east-2')
 with open('cf.yml','r') as f:
     mydata = yaml.load(f,Loader=yaml.FullLoader)
 
-myclient.run_instance(**mydata)
+myclient.create_bucket(**mydata)
 
 
 
