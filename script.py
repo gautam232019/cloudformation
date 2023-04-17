59e815a210b3ed5bd91b90971d6b2ec757d7d934
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-17 20:48:28 +0530

    tenth

---
+++ script.py
@@ -0,0 +1,19 @@
+import boto3
+import yaml
+
+myclient = boto3.client('s3',region_name='us-east-2')
+
+with open('cf.yml','r') as f:
+    mydata = yaml.load(f,Loader=yaml.FullLoader)
+
+myclient.run_instance(**mydata)
+
+
+
+
+
+
+
+
+
+
