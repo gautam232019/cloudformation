988d623cd3acb3a0ba91fa7b3815b8e2391a4af1
Gautam Sachdeva <56038800+gautam232019@users.noreply.github.com>
2023-04-17 21:27:18 +0530

    tenth

--- script.py
+++ script.py
@@ -1,14 +1,14 @@
 import boto3
 import yaml
 
-myclient = boto3.client('s3')
+# myclient = boto3.client('s3')
 
-with open('cf.yml','r') as f:
-    mydata = yaml.load(f,Loader=yaml.FullLoader)
+# with open('cf.yml','r') as f:
+#     mydata = yaml.load(f,Loader=yaml.FullLoader)
 
-bucket_name = mydata['BucketName']
-# location = config.get('Location','us-east-2')
-myclient.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={'LocationConstraint':'us-east-2'},)
+# bucket_name = mydata['BucketName']
+# # location = config.get('Location','us-east-2')
+# myclient.create_bucket(Bucket=bucket_name,CreateBucketConfiguration={'LocationConstraint':'us-east-2'},)
 
 
 myclient2 = boto3.client('ec2','us-east-2')
